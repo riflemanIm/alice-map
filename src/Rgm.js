484d2b294930c24dd981b51b1159f20546566068
Oleg <oleglambin@gmail.com>
2020-05-16 00:08:26 +0300

    CircleMarker

--- src/Rgm.js
+++ src/Rgm.js
@@ -9,19 +9,19 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
+//import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-//import img "./images/2175304.jpg";
+//import Button from "@material-ui/core/Button";
+import data from "./data";
 
-console.log("process.env.PUBLIC_URL", process.env.PUBLIC_URL);
+console.log("data", data);
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
   media: {
-    height: 140,
+    height: 345,
   },
 });
 //import { makeStyles } from "@material-ui/core/styles";
@@ -44,38 +44,31 @@ const MAP_OPTIONS = {
   clickableIcons: false,
   name: "cntr",
 };
-const markers = [
-  {
-    coords: {
-      // координаты
-      lat: 55.751244,
-      lng: 37.618423,
-    },
-    name: "Чертолье — бывшее капище Рода ", // незвание
-    description: "Особое место в славянской мифологии занимал..", // описание
-    imgLink: "http:// ...", // ссылка на фотку
-    audionLink: "http:// ...", // ссылка на аудио файл
-  },
-];
+
 export default function Rgm() {
   const classes = useStyles();
-  const onMarkerClick = (evt) => {
-    console.log("sssssssss");
-    toggleDrawer();
+  const api = useGoogleApiLoader();
+  const onMarkerClick = (evt, inx) => {
+    console.log("evt", evt, inx);
+    toggleDrawer(data[inx]);
   };
   //const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const toggleDrawer = (open) => {
-    setOpen(!open);
+  const [state, setOpen] = React.useState({});
+
+  const toggleDrawer = (newState) => {
+    setOpen({ ...newState, open: !state.open });
   };
   // const handleDrawerOpen = () => {
   //   setOpen(true);
   // };
 
   const handleDrawerClose = () => {
-    setOpen(false);
+    setOpen({ ...state, open: false });
   };
-  const api = useGoogleApiLoader();
+  React.useEffect(() => {
+    setOpen({ ...data[0], open: false });
+  }, []);
+
   return (
     <>
       <div
@@ -91,17 +84,20 @@ export default function Rgm() {
         {api && (
           <Map api={api} options={MAP_OPTIONS}>
             <Overlay>
-              <Marker lat={55.747347} lng={37.582135}>
-                <CircleMarker name="Чертолье" onClick={onMarkerClick} />
-              </Marker>
-              <Marker lat={55.744471} lng={37.605168}>
-                <CircleMarker name="cntr" onClick={onMarkerClick} />
-              </Marker>
+              {data.map((item, inx) => (
+                <Marker lat={item.coords.lat} lng={item.coords.lng} key={inx}>
+                  <CircleMarker
+                    name={item.name}
+                    onClick={(e) => onMarkerClick(e, inx)}
+                    acitive={state.name === item.name}
+                  />
+                </Marker>
+              ))}
             </Overlay>
           </Map>
         )}
       </div>
-      <Drawer anchor="right" open={open} onClose={handleDrawerClose}>
+      <Drawer anchor="right" open={state.open} onClose={handleDrawerClose}>
         <div
           css={css`
             min-midth: 230px;
@@ -111,34 +107,23 @@ export default function Rgm() {
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image="images/2175304.jpg"
-                title="Contemplative Reptile"
+                image={state.imgLink}
+                title={state.title}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Чертолье — бывшее капище Рода
+                  {state.title}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  Особое место в славянской мифологии занимал Род – отец богов.
-                  Капище этого бога считалось у древних славян чертой
-                  разделяющей светлые высшие и нижние темные миры. В Москве –
-                  это Чертолье, расположенное к западу от Кремля. Сегодня место
-                  капища Рода очерчено Чертольскими улицами, Волхонкой, Сивцевым
-                  Вражком и Власьевскими переулками. Вопреки популярной теории
-                  название улиц Чертолья произошли вовсе не от черта, а от черты
-                  отделяющей Навь и Явь. Навь — мир пращуров, предков, тех, кто
-                  хранит традиции Рода и Явь — мир людей. Скорее всего, капище
-                  находилось на дне оврага, по которому тёк ручей Черторый.
-                  Сегодня — это место, где Сивцев Вражек пересекается с Большим
-                  и Малым Власьевскими переулками.
+                  {state.description}
                 </Typography>
               </CardContent>
             </CardActionArea>
-            <CardActions>
+            {/* <CardActions>
               <Button size="small" color="primary">
                 Аудио
               </Button>
-            </CardActions>
+            </CardActions> */}
           </Card>
         </div>
       </Drawer>
@@ -146,21 +131,27 @@ export default function Rgm() {
   );
 }
 
-const CircleMarker = ({ name, onClick }) => (
+const CircleMarker = ({ name, onClick, acitive }) => (
   <div
     css={css`
       place-self: center center;
-      width: 30px;
-      height: 30px;
+      width: 50px;
+      height: 50px;
       border-radius: 100%;
-      background-color: white;
-      border: 1px solid red;
+      background-color: #d9d6d9;
+      background-size: contain;
+      background-image: url(images/ico${acitive ? "_mono" : ""}.svg);
+      background-position: center bottom;
+      background-repeat: no-repeat;
+      border: ${acitive ? 3 : 2}px solid #000;
       display: flex;
       align-items: center;
       justify-content: center;
+      opacity: ${acitive ? 1 : 0.8};
+      ${acitive
+        ? "box-shadow: 24px 28px 25px 0px rgba(128, 128, 128, 0.54);"
+        : ""}
     `}
     onClick={onClick}
-  >
-    {name}
-  </div>
+  />
 );
